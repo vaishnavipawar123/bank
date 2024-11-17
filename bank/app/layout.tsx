c570845249d4c3cc1c68d690e9fa81ab1a750382
Vaishnavi Pawar <vaishnavipawar860@example.com>
2024-11-17 19:16:54 +0530

    Initial commit

---
+++ bank/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import {Inter, IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets:['latin'],
+  weight:['400','700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "a modern banking platform for everyone",
+  icons:{ 
+    icon:'/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
